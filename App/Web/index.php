091903225507bdbe3faa9f523d297c0cc612fed7
Gilbert Paquin <gilbertpaquin1995@gmail.com>
2013-06-26 01:59:35 -0400

    Optimisation
    Ajout de 2 variable DEFINE DS = Directory Separator, ROOT = Le dossier du site

--- App/Web/index.php
+++ App/Web/index.php
@@ -1,10 +1,12 @@
 <?php
-
-    require_once "../../Lib/RatArt/Kernel/Autoloader.php";
+    define('ROOT', dirname(dirname(dirname(__FILE__))));
+    define('DS',DIRECTORY_SEPARATOR);
+    
+    require_once ROOT.DS."Lib".DS."RatArt".DS."Kernel".DS."Autoloader.php";
 
     $configs = json_decode(file_get_contents('App/Config/App.json'),true);
-     new RatArt\Kernel\App($configs);
+    new RatArt\Kernel\App($configs);
 
-     $Response = new RatArt\Kernel\Http\Response('Hello World !');
-     $Response->send();
+    $Response = new RatArt\Kernel\Http\Response('Hello World !');
+    $Response->send();
 
